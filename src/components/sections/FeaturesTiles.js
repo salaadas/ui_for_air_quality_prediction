6962f41787c89346369401bc1474e89787633aaa
salaadas <carbonfibersweeper@gmail.com>
2022-10-06 18:47:04 +0700

    ready set go

---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-tiles section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-tiles-inner section-inner pt-0',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap center-content',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Incorporating many technologies and design patterns',
+    paragraph: 'We use a variety of algorithms and libraries to predict the most accurate data about the air.'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/python-lang.png')}
+                      alt="Python"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Python
+                    </h4>
+                  <p className="m-0 text-sm">
+                    The main driver for the whole project.
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/panda-lib.jpg')}
+                      alt="Panda library"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Panda
+                    </h4>
+                  <p className="m-0 text-sm">
+                      For data analysis
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/keras-lib.png')}
+                      alt="Keras library"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Keras
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Interface for artificial neural network
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/numpy-lib.png')}
+                      alt="Numpy"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Numpy
+                    </h4>
+                  <p className="m-0 text-sm">
+                    For advanced mathematics tools
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/linear-regression.jpg')}
+                      alt="Linear Regression"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-16 mb-8">
+                    Linear Regression
+                    </h4>
+                  <p className="m-0 text-sm">
+                    
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-06.svg')}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Robust Workflow
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                    </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
---
+++ src/components/sections/Hero.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { SectionProps } from '../../utils/SectionProps';
+import ButtonGroup from '../elements/ButtonGroup';
+import Button from '../elements/Button';
+import Image from '../elements/Image';
+import Modal from '../elements/Modal';
+
+const propTypes = {
+    ...SectionProps.types
+}
+
+const defaultProps = {
+    ...SectionProps.defaults
+}
+
+const Hero = ({
+    className,
+    topOuterDivider,
+    bottomOuterDivider,
+    topDivider,
+    bottomDivider,
+    hasBgColor,
+    invertColor,
+    ...props
+}) => {
+
+    const [videoModalActive, setVideomodalactive] = useState(false);
+
+    const openModal = (e) => {
+        e.preventDefault();
+        setVideomodalactive(true);
+    }
+
+    const closeModal = (e) => {
+        e.preventDefault();
+        setVideomodalactive(false);
+    }   
+
+    const outerClasses = classNames(
+        'hero section center-content',
+        topOuterDivider && 'has-top-divider',
+        bottomOuterDivider && 'has-bottom-divider',
+        hasBgColor && 'has-bg-color',
+        invertColor && 'invert-color',
+        className
+    );
+
+    const innerClasses = classNames(
+        'hero-inner section-inner',
+        topDivider && 'has-top-divider',
+        bottomDivider && 'has-bottom-divider'
+    );
+
+    return (
+        <section
+            {...props}
+            className={outerClasses}
+        >
+            <div className="container-sm">
+                <div className={innerClasses}>
+                    <div className="hero-content">
+                        <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
+                            <span className="text-color-primary">Air Quality</span> Index Predict
+                        </h1>
+                        <div className="container-xs">
+                            <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+                                More and more people are aware of the air polution, and our goal is to provide <span className="text-color-primary">accurate</span> predictions using ML.
+                            </p>
+                            <div className="reveal-from-bottom" data-reveal-delay="600">
+                                <ButtonGroup>
+                                    <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
+                                        Learn more
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hero-figure reveal-from-bottom" data-reveal-value="20px" data-reveal-delay="800">
+                        TODO: Them hinh air quality qua cac nam
+                        <Image
+                            className="has-shadow"
+                            src={require('./../../assets/images/video-placeholder.jpg')}
+                            alt="Hero"
+                            width={896}
+                            height={504} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
