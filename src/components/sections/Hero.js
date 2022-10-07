af52b617de3b35a0beae764f952c05be1d44538f
salaadas <carbonfibersweeper@gmail.com>
2022-10-07 14:42:52 +0700

    last one

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -69,7 +69,7 @@ const Hero = ({
                             </p>
                             <div className="reveal-from-bottom" data-reveal-delay="600">
                                 <ButtonGroup>
-                                    <Button tag="a" color="primary" wideMobile href="https://google.com">
+                                    <Button tag="a" color="primary" wideMobile href="#predictions">
                                         Try out the prediction
                                     </Button>
                                 </ButtonGroup>
